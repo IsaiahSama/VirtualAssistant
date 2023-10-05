ca25565863503c5685bcb97534f0384d01b8688b
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-10-05 19:57:38 -0400

    Added Utils file

--- static/index.js
+++ static/index.js
@@ -1,27 +1,3 @@
-const assistant = document.getElementById("assistant");
-const assistantMessage = document.getElementById("assistantMessage");
-
-const states = {
-  Idle: "idle",
-  Talking: "talking",
-  Roaming: "roaming",
-};
-
-const changeState = (newState) => {
-  assistant.className = newState;
-};
-
-const typeMessage = (char) => {
-  assistantMessage.innerHTML += char;
-};
-
-const sendMessage = (message) => {
-  assistantMessage.innerText = "";
-  for (let i in message) {
-    setTimeout(typeMessage, 100 * i, message[i]);
-  }
-};
-
 const convoStarter = () => {};
 const emoSupport = () => {};
 const moodBoost = () => {};
---
+++ static/utils.js
@@ -0,0 +1,23 @@
+const assistant = document.getElementById("assistant");
+const assistantMessage = document.getElementById("assistantMessage");
+
+const states = {
+  Idle: "idle",
+  Talking: "talking",
+  Roaming: "roaming",
+};
+
+const changeState = (newState) => {
+  assistant.className = newState;
+};
+
+const typeMessage = (char) => {
+  assistantMessage.innerHTML += char;
+};
+
+const sendMessage = (message) => {
+  assistantMessage.innerText = "";
+  for (let i in message) {
+    setTimeout(typeMessage, 100 * i, message[i]);
+  }
+};
