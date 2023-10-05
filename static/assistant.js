85fef2e651a8d440d5bd628125ccc6c8ca6219ef
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-10-05 19:20:36 -0400

    Added various animations for the character

--- static/assistant.js
+++ static/assistant.js
@@ -1,6 +1,8 @@
-const assistant = document.getElementById("assisstant");
+const assistant = document.getElementById("assistant");
 const states = {
   Idle: "idle",
   Talking: "talking",
   Roaming: "roaming",
 };
+
+let state = states.Idle;
