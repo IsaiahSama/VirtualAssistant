820ac9e9dc56733d87d5f96b7144ed47ced4430e
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-10-07 17:38:22 -0400

    Extracted into extra

--- static/utils.js
+++ static/utils.js
@@ -104,40 +104,6 @@ const getFact = async () => {
   return json[0]["fact"];
 };
 
-const prompts = [
-  "Life is a beautiful journey.",
-  "Do you believe in magic?",
-  "What's your favorite book?",
-  "Isn't technology amazing?",
-  "The world is full of wonder.",
-  "Music can heal the soul.",
-  "Have you ever traveled abroad?",
-  "Every day is a new adventure.",
-  "Kindness goes a long way.",
-  "Dreams can come true.",
-  "Laughter is the best medicine.",
-  "What's your favorite movie?",
-  "Cherish the moments that matter.",
-  "Do you have a favorite quote?",
-  "Nature is so peaceful.",
-  "Smile, it's contagious!",
-  "Have you ever tried meditation?",
-  "Art can be so inspiring.",
-  "What's your favorite hobby?",
-  "Love makes the world go 'round.",
-  "Have you ever seen a shooting star?",
-  "Be yourself, everyone else is taken.",
-  "Sunsets are pure magic.",
-  "Learning is a lifelong journey.",
-  "What's your favorite food?",
-  "The little things matter most.",
-  "Dance like nobody's watching.",
-  "Have you ever been to a concert?",
-  "Family is everything.",
-  "The universe has a plan for you.",
-  "Adventure is out there!",
-];
-
 const beIdle = () => {
   if (
     assistant.className == States.Idle ||
