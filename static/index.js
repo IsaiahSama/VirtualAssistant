bd7f1e34f23caa9936bfb2a18b2bde352a201ed7
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-10-07 16:42:52 -0400

    Fixed issue with overwriting text

--- static/index.js
+++ static/index.js
@@ -1,24 +1,41 @@
+let handling = false;
+
+const grabHandling = () => {
+  if (handling == true) return false;
+  handling = true;
+  setTimeout(() => {
+    handling = false;
+  }, 5000);
+  return true;
+};
+
 const convoStarter = () => {
+  if (grabHandling() == false) return false;
   changeState(States.Vibing);
   getFact().then((fact) => sendMessage(fact));
 };
 const emoSupport = () => {
+  if (grabHandling() == false) return false;
   changeState(States.Talking);
 
   getQuote().then((quote) => sendMessage(quote));
 };
 const moodBoost = () => {
+  if (grabHandling() == false) return false;
   changeState(States.Talking);
   getJoke().then((joke) => sendMessage(joke));
 };
 const entertainment = () => {
+  if (grabHandling() == false) return false;
   changeState(States.Happy);
   getTask().then((task) => sendMessage(task));
 };
 const feed = () => {
+  if (grabHandling() == false) return false;
   changeState(States.Happy, 5);
   sendMessage("Nom noms");
 };
 const help = () => {
-  alert("Press buttons");
+  if (grabHandling() == false) return false;
+  sendMessage("Press buttons");
 };
