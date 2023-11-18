7c53faef6827e897e317ec44f1a86100b135e849
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-11-18 15:15:16 -0400

    Can now update messages

--- static/scripts/extra.js
+++ static/scripts/extra.js
@@ -2,6 +2,7 @@ const inputField = document.getElementById("userInput");
 
 const handleInput = (e) => {
   if (e.keyCode === 13) {
+    updateMessages("user", inputField.value);
     inputField.value = " ";
   }
 };
